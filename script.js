8507d4bbed33b04d2b8874091d3700da10c9dd5c
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-02-24 16:09:34 +0000

    Implement getHumanChoice function to return 'rock', 'paper', or 'scissors' based on user input

--- script.js
+++ script.js
@@ -5,7 +5,22 @@ function getComputerChoice(){
         return "Rock";
     }
 
-    else if  (Computer_Choice==1){
+    else if  (Computer_Choice===1){
+        return "Paper";
+    }
+
+    else{
+        return "Scissors";
+    }
+}
+
+function getHumanChoice(){
+    const Human_Choice=window.prompt("what is your choise ? (pick one number) \n 1- Rock \n 2- Paper \n 3- Scissors");
+    if (Human_Choice==1){
+        return "Rock";
+    }
+
+    else if  (Human_Choice==2){
         return "Paper";
     }
 
